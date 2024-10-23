e3ebe1e8739157691ac1cec4ab0e31a3ac54f319
Zimmer <20681015+zimmerx@users.noreply.github.com>
2024-10-23 15:03:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,30 @@
+provider "aws" {
+  region = "ca-central-1" # Adjust to your AWS region
+}
+
 module "cloudwatch_dashboard" {
-  source         = "../modules/cloudwatch_dashboard"
+  source         = ".//modules/cloudwatch_dashboard"
   dashboard_name = "GFL_Dashboard"
   aws_region     = "ca-central-1" # Pass the region
+
+  #EC2 instances ID
+  ec2_instance_ids = ["i-04eb4680746fbadd7", "i-03e0e8f946d8128df"] # to skip the service leave it empty 
+  # Example ec2_instance_ids  = ["i-0123456789abcdef0", "i-0987654321abcdef0"]
+
+  # ECS services and cluster names
+  ecs_services = [] # to skip the service leave it empty 
+  # Example for ecs_services: 
+  # ecs_services = [
+  #  { cluster_name = "my-ecs-cluster", service_name = "my-ecs-service1" },
+  # { cluster_name = "my-ecs-cluster", service_name = "my-ecs-service2" }
+  # ]
+
+  # S3 buckets
+  s3_bucket_names = ["upload-win-dt-flow"] # to skip the service leave it empty 
+  # Example  s3_bucket_names = ["my-s3-bucket1", "my-s3-bucket2"]
+
+
+  # RDS instances
+  rds_instance_ids = [] # to skip the service leave it empty 
+  # Example rds_instance_ids = ["my-rds-instance1", "my-rds-instance2"]
 }
