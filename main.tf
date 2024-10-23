533ad7409aa424c13f36dbd05f6b9100199602b6
Zimmer <20681015+zimmerx@users.noreply.github.com>
2024-10-23 14:50:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cloudwatch_dashboard" {
-  source         = ".//modules/cloudwatch_dashboard"
+  source         = "../modules/cloudwatch_dashboard"
   dashboard_name = "GFL_Dashboard"
   aws_region     = "ca-central-1" # Pass the region
 }
